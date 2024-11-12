e86b79a8b7e2486ad1345d4941aa9171f5f2965b
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-11-12 22:32:43 +0700

    init commit

---
+++ src/main/java/ru/netology/ntlg_hw_springboot_rest/NtlgHwSpringBootRestApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.ntlg_hw_springboot_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtlgHwSpringBootRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NtlgHwSpringBootRestApplication.class, args);
+    }
+
+}
