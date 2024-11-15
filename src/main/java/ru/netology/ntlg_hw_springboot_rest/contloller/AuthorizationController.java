adf46b0af179d20c910433d7b16b67365bda6712
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-11-16 03:46:52 +0700

    change Controller to catch Exception

--- src/main/java/ru/netology/ntlg_hw_springboot_rest/contloller/AuthorizationController.java
+++ src/main/java/ru/netology/ntlg_hw_springboot_rest/contloller/AuthorizationController.java
@@ -1,10 +1,15 @@
 package ru.netology.ntlg_hw_springboot_rest.contloller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.netology.ntlg_hw_springboot_rest.domain.Authorities;
+import ru.netology.ntlg_hw_springboot_rest.exception.InvalidCredentials;
+import ru.netology.ntlg_hw_springboot_rest.exception.UnauthorizedUser;
 import ru.netology.ntlg_hw_springboot_rest.service.AuthorizationService;
 
 import java.util.List;
@@ -22,5 +27,20 @@ public class AuthorizationController {
     public List<Authorities> getAuthorities(@RequestParam("user") String user,
                                             @RequestParam("password") String password) {
         return service.getAuthorities(user, password);
+    }
+
+    @ExceptionHandler(InvalidCredentials.class)
+    public ResponseEntity<String> icEx(InvalidCredentials e) {
+        return new ResponseEntity<>("Status code: " + HttpStatus.BAD_REQUEST + "\n" +
+                "Message: " + e.getMessage(),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UnauthorizedUser.class)
+    public ResponseEntity<String> uuEx(UnauthorizedUser e) {
+        System.out.println(e.getMessage());
+        return new ResponseEntity<>("Status code: " + HttpStatus.UNAUTHORIZED + "\n" +
+                "Message: " + e.getMessage(),
+                HttpStatus.UNAUTHORIZED);
     }
 }
--- src/main/java/ru/netology/ntlg_hw_springboot_rest/exception/InvalidCredentials.java
+++ src/main/java/ru/netology/ntlg_hw_springboot_rest/exception/InvalidCredentials.java
@@ -1,7 +1,5 @@
 package ru.netology.ntlg_hw_springboot_rest.exception;
 
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public class InvalidCredentials extends RuntimeException {
     public InvalidCredentials(String msg) {
         super(msg);
